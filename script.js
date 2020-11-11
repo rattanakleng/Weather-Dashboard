f4788b12083c205a7e3dd550e123da26335d6ea0
Rattanak Leng <rleng@live.com>
2020-11-11 15:20:46 -0800

    add initial fils

---
+++ script.js
@@ -0,0 +1,76 @@
+// ```
+// AS A traveler
+// I WANT to see the weather outlook for multiple cities
+// SO THAT I can plan a trip accordingly
+// ```
+
+// ## Acceptance Criteria
+
+// ```
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
+// ```
+
+// ```
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+    // var inputValue
+    // var cardCardCtn
+    // var searchBtn
+    // var historySearchCtner
+    // var subCard
+
+    var
+
+// Create API request
+    // var url
+    // var apiKey
+    // var response
+
+    // Function apiRequest
+
+// Add eventListener to searchBtn to run all function
+
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+    // Function add btn to historySearchCtner
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+    // Function displayCurrent weather condition temperature, humidity, wind-speed, and UV index-with-color-indication
+
+
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+
+    // funtion display fiveDayforcus
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+    // Add eventListener searchHistoryBtn
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
+
+    // function display last searched city
+
+
+// ```
+
+
